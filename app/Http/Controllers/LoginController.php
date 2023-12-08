c7ffc4546e60a34139fd1ed54e746effbbcbe5c5
Sampan Verma <sampan.developer@gmail.com>
2023-12-09 00:39:59 +0530

    Rename variable name to identifier

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,18 +8,24 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public string $attemptMessage = "This credentials combination doesn't exist. <br /> Please try again.";
+
     public function store(Request $request){
         $validatedRequest = $request->validate([
             "username" => "required|string",
             "password" => "required|string"
         ]);
-        $user = User::where("username", $validatedRequest["username"])->orWhere("email", $validatedRequest["username"])->first();
-        if(!$user){
+        $identifier = $validatedRequest["username"];
+        $user = User::where("username", $identifier)
+                    ->orWhere("email", $identifier)
+                    ->first();
+
+        if (!$user) {
             return redirect()->back()->with("error", $this->attemptMessage);
         }
         auth()->login($user);
         return redirect()->route('home');
     }
+
     public function view(){
         return view('login');
     }
